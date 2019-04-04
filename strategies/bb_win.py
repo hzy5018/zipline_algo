1f3dde46bd3e0a23944a365f0e7e6e14d182b905
zhiyuan.hu <chiyuen_woo@163.com>
2019-04-04 14:37:26 +0800

    update

--- strategies/bb_win.py
+++ strategies/bb_win.py
@@ -39,7 +39,7 @@ def initialize(context):
     )
 
     # Create our pipeline and attach it to our algorithm.
-    my_pipe = make_pipeline(10, 2)
+    my_pipe = make_pipeline(20, 2)
     attach_pipeline(my_pipe, 'my_pipeline')
 
 
